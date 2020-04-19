5450cffcf4d6ab6ae91d214a9daa876b1544ecb1
tanya9020 <43775063+tanya9020@users.noreply.github.com>
2020-04-19 19:28:00 +0530

    Add files via upload

---
+++ flipping_bits.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    unsigned long long int a=4294967295,b;
+   
+    while(n--)
+        {
+        cin>>b;
+        cout<<a-b<<endl;
+   }
+    return 0;
+}
+
