90990cc9c115590e90468726a1f0110d3c833227
tanya9020 <43775063+tanya9020@users.noreply.github.com>
2020-04-19 11:18:30 +0530

    Add files via upload

---
+++ min_abs_diff_in_array.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+def minimumAbsoluteDifference(arr):
+    diffs = []
+    arr.sort()
+    for i in range(len(arr)-1):
+        diffs.append(abs(arr[i]-arr[i+1]))
+    return min(diffs)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = minimumAbsoluteDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
