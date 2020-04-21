0461e8e46e1de0334ba65352d0a66448d7581e6b
tanya9020 <43775063+tanya9020@users.noreply.github.com>
2020-04-21 18:47:00 +0530

    Add files via upload

---
+++ Project_Euler_#13_Large_sum.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import sys
+n=int(input())
+sum=0
+
+for j in range (n):
+    temp=int(input())
+    sum+=temp
+
+sumt=str(sum)
+print(sumt[0:10])    
+
+
+
