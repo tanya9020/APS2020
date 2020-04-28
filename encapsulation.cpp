c24b1dae0376042ad222f8b633671ea6784122b4
tanya9020 <43775063+tanya9020@users.noreply.github.com>
2020-04-28 20:32:34 +0530

    Add files via upload

---
+++ encapsulation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+class sample{
+    
+    private:
+        int a;
+        int b;
+
+    public:
+        void set(int x,int y){
+            a=x;
+            b=y;
+        }
+
+        void get(){
+            cout<<a<<"::"<<b<<"\n";
+        }
+};
+
+int main(){
+    sample s;
+
+    s.set(10,20);
+    
+    s.get();
+    
+    return 0;
+}
